88b464b08d598f554596d30130be577b087b5007
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-01-24 22:50:02 +0530

    Fix bugs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,9 +6,16 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-    <View className="flex-1 items-center justify-center bg-white">
-    <Text>Open up App.js to start working on your app!</Text>
-    <StatusBar  />
+    <View className="flex-1 items-center justify-center bg-black">
+    <Text className='text-red-500'>Open up App.js to start working on your app!</Text>
+    <Text className='text-red-500'>Open up App.js to start working on your app!</Text>
+    <Text className='text-red-500'>Open up App.js to start working on your app!</Text>
+    <Text className='text-red-500'>Open up App.js to start working on your app!</Text>
+    <Text className='text-red-500'>Open up App.js to start working on your app!</Text>
+    <Text className='text-red-500'>Open up App.js to start working on your app!</Text>
+    <Text className='text-red-500'>Open up App.js to start working on your app!</Text>
+    <Text className='text-red-500'>Open up App.js to start working on your app!</Text>
+    <StatusBar/>
   </View>
   </SafeAreaView>
 
